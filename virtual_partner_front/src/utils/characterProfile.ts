58d088b1a499032e19f55d3808ce26f31510e6b7
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-27 21:37:59 +0900

    fix: プロフィールページの作成

---
+++ virtual_partner_front/src/utils/characterProfile.ts
@@ -0,0 +1,33 @@
+/* 複数のキャラクターのプロフィールを管理する */
+
+export const characterProfiles = [
+  {
+    name: "桃瀬ひより",
+    age: "14",
+    firstPerson: "私",
+    bloodType: "A",
+    birthDate: "9月18日",
+    favoriteFood: "プチトマト",
+    club: "園芸部",
+    hobbies: "家庭菜園",
+    height: "145cm",
+    description: "癒し系妹キャラやってます!!",
+    imageUrl: "character/hiyori/p-hiyori.png",
+    bgColor: "bg-yellow-100",
+  },
+  {
+    name: "ずんだもん",
+    age: "年齢の概念なし",
+    firstPerson: "ボク",
+    bloodType: "不明",
+    birthDate: "12月5日",
+    favoriteFood: "ずんだ餅",
+    club: "なし",
+    hobbies: "その辺をふらふらすること",
+    height: "不明",
+    description:
+      "ずんだ餅をモチーフにした妖精で、東北ずん子の関連キャラクター。語尾に「～なのだ」をつけて話すのが特徴。",
+    imageUrl: "character/zundamon/p-zundamon.png",
+    bgColor: "bg-green-100",
+  },
+];
