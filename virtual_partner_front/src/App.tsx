3cb775230f734fc6998c305c35705ea24f6eb0d9
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-27 21:48:51 +0900

    fix: react-transition-groupの適用

--- virtual_partner_front/src/App.tsx
+++ virtual_partner_front/src/App.tsx
@@ -1,13 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home'
-import { Memory } from './pages/Memory'
-import { Profile } from './pages/Profile'
-import { Extra } from './pages/Extra'
-import { Development } from './pages/Development'
-import { useEffect, useState } from 'react'
-import { Loading } from './components/Loading'
-import { Footer } from './features/Nuvigation/Footer'
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Memory } from "./pages/Memory";
+import { Profile } from "./pages/Profile";
+import { Extra } from "./pages/Extra";
+import { Development } from "./pages/Development";
+import { useEffect, useState } from "react";
+import { Loading } from "./components/Loading";
+import { Footer } from "./features/Nuvigation/Footer";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const AppContent = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={500}>
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/memory" element={<Memory />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/extra" element={<Extra />} />
+          <Route path="/development" element={<Development />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -18,33 +37,33 @@ function App() {
       setLoading(false);
     };
 
-    window.addEventListener('load', handleLoad);
+    window.addEventListener("load", handleLoad);
 
     return () => {
-      window.removeEventListener('load', handleLoad);
+      window.removeEventListener("load", handleLoad);
     };
   }, []);
 
   return (
-    <div className={`transition-opacity duration-1000 ${loading ? 'opacity-0' : 'opacity-100'}`}>
+    <div
+      className={`transition-opacity duration-1000 ${
+        loading ? "opacity-0" : "opacity-100"
+      }`}
+    >
       {loading ? (
         <Loading />
       ) : (
         <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/memory' element={<Memory />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/extra' element={<Extra />} />
-            <Route path='/development' element={<Development />} />
-          </Routes>
-          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8 w-full max-w-md z-40">
-            <Footer />
+          <div className="relative min-h-screen">
+            <AppContent />
+            <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-8 w-full max-w-md z-40">
+              <Footer />
+            </div>
           </div>
         </BrowserRouter>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
