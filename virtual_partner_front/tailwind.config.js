1c83f6c4778775cdbff36a12a2521e62c251400d
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-27 18:07:45 +0900

    feat: niconicoコメント用関数のCSSの作成

--- virtual_partner_front/tailwind.config.js
+++ virtual_partner_front/tailwind.config.js
@@ -8,9 +8,20 @@ export default {
     extend: {
       fontFamily: {
         kiwi: ['Kiwi Maru', 'sans-serif'],
-      }
+      },
+      backgroundImage: {
+        'custom-image': "url('/background/beach_station.jpg')",
+      },
+      keyframes: {
+        'niconico-scroll': {
+          '0%': { transform: 'translateX(400%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'niconico-scroll': 'niconico-scroll 5s linear forwards',
+      },
     },
   },
   plugins: [],
 }
-
