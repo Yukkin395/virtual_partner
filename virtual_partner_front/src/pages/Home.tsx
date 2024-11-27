f6c0fb47fe428bb589fa7d796cf9b32009fd61f0
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-27 18:10:10 +0900

    feat: Homeに作成したUIコンポーネントを表示

--- virtual_partner_front/src/pages/Home.tsx
+++ virtual_partner_front/src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import { TextField } from "../components/TextField"
 import Live2DModelComponent from "../features/Live2D/components/Live2DModelComponent"
-
+import { NiconicoView } from "../features/Niconico/NiconicoView"
+import { TalkBoxView } from "../features/TalkBox/TalkBoxView"
 
 const Home = () => {
   return (
-    <div>
+    <div className="relative min-h-screen">
       <Live2DModelComponent audioUrl="/hello.wav" />
+      <div className="absolute top-0 left-0 m-4">
+        <TalkBoxView />
+      </div>
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8 w-full max-w-md">
+        <TextField placeholder="テキストを入力してください" />
+      </div>
+      <div className="absolute inset-0 pointer-events-none">
+        <NiconicoView />
+      </div>
     </div>
   )
 }
