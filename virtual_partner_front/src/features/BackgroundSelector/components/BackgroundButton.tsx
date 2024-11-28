5194da4abe12e4db5e7e511a40e4dff53cb4458e
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-29 00:22:22 +0900

    fix: 背景を切り替えるボタンの背景を白に変更

--- virtual_partner_front/src/features/BackgroundSelector/components/BackgroundButton.tsx
+++ virtual_partner_front/src/features/BackgroundSelector/components/BackgroundButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const BackgroundButton = ({ onClick }: Props) => (
   <button
     onClick={onClick}
-    className="fixed z-50 p-2 bg-white backdrop-blur-sm rounded-lg shadow-lg hover:bg-white/40 transition-colors"
+    className="fixed z-50 p-2 bg-white backdrop-blur-sm rounded-lg shadow-lg hover:scale-105 transition-colors"
   >
     <Image className="h-6 w-6" />
   </button>
