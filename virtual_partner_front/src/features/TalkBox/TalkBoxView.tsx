d5069a049b2aa2cb68c08789714a5c6f2a739b82
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-29 16:33:01 +0900

    feat: 会話用のボックスの作成

--- virtual_partner_front/src/features/TalkBox/TalkBoxView.tsx
+++ virtual_partner_front/src/features/TalkBox/TalkBoxView.tsx
@@ -1,68 +1,20 @@
-import { Avatar } from "@mui/material";
-import { useState } from "react";
-
-type Message = {
-  sender: "user" | "ai";
-  text: string;
+type TalkBoxViewProps = {
+  message: string;
 };
-export const TalkBoxView = () => {
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      sender: "user",
-      text: "今日は疲れたね〜",
-    },
-    {
-      sender: "ai",
-      text: "そうだね。ゆっくり休もう！",
-    },
-    {
-      sender: "user",
-      text: "もうすぐ夏休みだし、どこか行きたいな〜",
-    },
-    {
-      sender: "ai",
-      text: "海に行くのはどうかな？",
-    },
-  ]);
 
-  console.log(setMessages);
+export const TalkBoxView = ({ message }: TalkBoxViewProps) => {
+  const borderColor = "border-gray-300";
 
   return (
-    <div className="p-4 bg-pink-50 rounded-2xl max-w-md mx-auto shadow-lg">
-      <div className=" bg- flex justify-center items-center">
-        <h2 className="text-lg font-bold mb-4">思い出</h2>
-      </div>
-      {messages.map((message, index) => (
-        <div
-          key={index}
-          className={`mb-4 flex items-center ${
-            message.sender === "user" ? "justify-start" : "justify-end"
-          }`}
-        >
-          {message.sender === "user" && (
-            <Avatar
-              sx={{ width: 32, height: 32 }}
-              className="mr-2"
-            />
-          )}
-          <div
-            className={`p-3 rounded-2xl max-w-[75%] ${
-              message.sender === "user"
-                ? "bg-blue-100 text-gray-700 rounded-bl-none"
-                : "bg-yellow-100 text-gray-700 rounded-br-none"
-            } shadow-sm`}
-          >
-            {message.text}
-          </div>
-          {message.sender === "ai" && (
-            <Avatar
-              src="/icon/hiyori.jpg"
-              sx={{ width: 32, height: 32 }}
-              className="ml-2"
-            />
-          )}
-        </div>
-      ))}
+    <div
+      className={`relative bg-white bg-opacity-90 rounded-2xl ${borderColor} shadow-xl min-w-[240px] max-w-[400px] mx-auto py-4 px-8`}
+    >
+      <p className="text-[#865734] text-lg leading-relaxed font-bold tracking-wide">
+        {message}
+      </p>
+      <div
+        className={`absolute top-[-10px] left-[60%] transform -translate-x-1/2 w-0 h-0 border-b-[10px] border-b-white border-b-opacity-90 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent ${borderColor}`}
+      ></div>
     </div>
   );
 };
